77ef6ebfa3cb53c8caccc3f9767498b98b1fe4df
SagarOad <124421949+SagarOad@users.noreply.github.com>
2024-03-25 04:03:37 -0900

    New changes

--- src/Sections/Process.js
+++ src/Sections/Process.js
@@ -30,9 +30,21 @@ const Process = () => {
             </p>
           </div>
           <div className="col-span-12  m-2 rounded-3xl bg-[#009fd4] md:col-span-3 flex p-12 flex-col justify-center items-center">
-            <BsRocket className=" text-[86px] mb-1 text-white" />
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 24 24"
+              class=" text-[92px] mx-auto mb-8 text-[#ffffff] group-hover:text-white"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path fill="none" d="M0 0h24v24H0z"></path>
+              <path d="M21 10.12h-6.78l2.74-2.82c-2.73-2.7-7.15-2.8-9.88-.1-2.73 2.71-2.73 7.08 0 9.79s7.15 2.71 9.88 0C18.32 15.65 19 14.08 19 12.1h2c0 1.98-.88 4.55-2.64 6.29-3.51 3.48-9.21 3.48-12.72 0-3.5-3.47-3.53-9.11-.02-12.58s9.14-3.47 12.65 0L21 3v7.12zM12.5 8v4.25l3.5 2.08-.72 1.21L11 13V8h1.5z"></path>
+            </svg>
             <h2 className=" text-center text-white leading-10 mb-2 font-bold text-[1.9rem]">
-              Delivery
+              REVIEW & TEST
             </h2>
             <p className="leading-normal text-white mt-3 text-center text-[18px] md:text-[18px]">
               On-time delivery is our best service. Our healthy work environment
--- src/app/services/web-dev/page.js
+++ src/app/services/web-dev/page.js
@@ -151,7 +151,7 @@ const page = () => {
               <path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm0 10.99h7c-.53 4.12-3.28 7.79-7 8.94V12H5V6.3l7-3.11v8.8z"></path>
             </svg>
             <h2 className=" text-center text-white leading-10 mb-2 font-bold text-[1.9rem]">
-              Executive
+              Execution
             </h2>
             <p className="leading-normal text-white mt-3 text-center text-[18px] md:text-[18px]">
               We are equipped with skilled people and state-of-the-art rendering
@@ -173,7 +173,7 @@ const page = () => {
               <path d="M21 10.12h-6.78l2.74-2.82c-2.73-2.7-7.15-2.8-9.88-.1-2.73 2.71-2.73 7.08 0 9.79s7.15 2.71 9.88 0C18.32 15.65 19 14.08 19 12.1h2c0 1.98-.88 4.55-2.64 6.29-3.51 3.48-9.21 3.48-12.72 0-3.5-3.47-3.53-9.11-.02-12.58s9.14-3.47 12.65 0L21 3v7.12zM12.5 8v4.25l3.5 2.08-.72 1.21L11 13V8h1.5z"></path>
             </svg>
             <h2 className=" text-center text-white leading-10 mb-2 font-bold text-[1.9rem]">
-              Delivery
+              REVIEW & TEST
             </h2>
             <p className="leading-normal text-white mt-3 text-center text-[18px] md:text-[18px]">
               On-time delivery is our best service. Our healthy work environment
